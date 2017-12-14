5a80d38dc3af0db9eb7521655743b877ca013482
roadrunner <alphadog500@gmail.com>
2017-12-14 22:29:47 +0100

    update

---
+++ src/store/reducer.js
@@ -0,0 +1,31 @@
+import * as actionTypes from './actionTypes';
+
+const initialState = {
+	ingredients: {
+		salad: 0,
+		bacon : 0,
+		cheese: 0,
+		meat: 0
+	},
+	price: 0
+};
+
+
+const reducer = (state = initialState, action) => {
+	switch (action.type) {
+		case actionTypes.ADD_INGREDIENT:
+			return {...state, ingredients: {
+				...state.ingredients,
+				[action.ingredientName] : state.ingredients[action.ingredientName] + 1
+			}};
+		case actionTypes.REMOVE_INGREDIENT:
+			return {...state, ingredients: {
+				...state.ingredients,
+				[action.ingredientName] : state.ingredients[action.ingredientName] - 1
+			}};
+		default:
+			return state;
+	}
+};
+
+export  default reducer;
